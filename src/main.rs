115f2535b9f7338ee2ee118f7a34c27a1baab149
r31oad3d <r31oad3d@163.com>
2020-10-19 21:56:20 +0800

    20201019

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,5 @@
 mod data_structure;
-#[macro_use]
-mod macros;
+
 
 use data_structure::binary_tree;
 use data_structure::queue::Queue;
@@ -26,46 +25,10 @@ fn test_stack() {
 
 fn test_queue() {}
 
-struct Dummy(i32);
 
-impl Dummy {
-    double_method! {
-        self,
-        {
-            self.0 *= 2;
-            self
-        }
-    }
-}
 
 fn main() {
     test_stack();
 
-    //let color = vec![RED,GREEN, BLUE];
-    let a = 3 * four!();
-    let b = multiply_add!(1, 2, 3);
-    println!("a={}", a);
-    println!("b={}", b);
-    let v = vec_strs![1 + 1, 2 + 2];
-    assert_eq!(&*v, &["2", "4"]);
-    println!("{:?}", stringify!(dummy(2 * (1 + (3)))));
-    println!("{:?}", capture_expr_then_stringify!(dummy(2 * (1 + (3)))));
 
-    println!(
-        "{}\n{}\n{}\n",
-        match_tokens!((caravan)),
-        match_tokens!(3 + 6),
-        match_tokens!(5)
-    );
-    println!(
-        "{}\n{}\n{}",
-        capture_then_match_tokens!((caravan)),
-        capture_then_match_tokens!(3 + 6),
-        capture_then_match_tokens!(5)
-    );
-
-    //let four = using_a1!(a1/10);
-
-    println!("{}", what_is!(self));
-    println!("{}", call_with_ident!(what_is(self)));
 }
